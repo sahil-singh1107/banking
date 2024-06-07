9f447fc523a1c5102c136ed7e60d5463f1eab4bf
sahil singh <sahilpython@gmail.com>
2024-06-07 20:50:26 +0530

    implement complete ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import RightSidebar from '@/components/ui/RightSidebar'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn = {firstName: 'Sahil', lastName: 'Singh', email: 'sahil@gmail.com'}
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user= {loggedIn?.firstName || 'Guest'}
+            subtext = "Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance:123.50}]} />
+    </section>
   )
 }
 
